212a0bb73c84e03caa448da1631e272e07c1cffe
luowei <lw54760187@hotmail.com>
2017-04-14 17:11:25 +0800

    angualr parser

---
+++ src/parse.js
@@ -0,0 +1,327 @@
+'use strict';
+
+var _ = require('lodash');
+
+var ESCAPES = {
+	'n': '\n',
+	'f': '\f',
+	'r': '\r',
+	't': '\t',
+    'v': '\v',
+    '\'': '\'',
+    '"': '"'
+};
+
+/* 词法分类器 */
+function Lexer(text) {
+	
+}
+
+Lexer.prototype.lex = function(text) {
+	this.text = text;
+	this.index = 0;
+	this.ch = undefined;
+	this.tokens = [];
+
+	while(this.index < this.text.length) {
+		this.ch = this.text.charAt(this.index);
+
+		if(this.isNumber(this.ch) || (this.ch === '.' && this.isNumber(this.peek()))) {
+			this.readNumber();
+		} else if(this.ch === '\'' || this.ch === '"') {
+			this.readString(this.ch);
+		} else if(this.ch === '[' || this.ch === ']') {
+			this.tokens.push({
+				text: this.ch
+			});
+			this.index++;
+		} else if(this.isIdent(this.ch)) {
+			this.readIdent();
+		} else if(this.isWhitespace(this.ch)) {
+			this.index++;
+		} else {
+			throw 'Unexpected next character: ' + this.ch;
+		}
+	}
+
+	return this.tokens;
+};
+
+Lexer.prototype.isNumber = function(ch) {
+	return '0' <= ch && ch <= '9';
+};
+
+/* 对number进行词法分析，提供给AST builder */
+Lexer.prototype.readNumber = function() {
+	var number = '';
+
+	while(this.index < this.text.length) {
+		var ch = this.text.charAt(this.index).toLowerCase();
+
+		if(ch === '.' || this.isNumber(ch)) {
+			number += ch;
+		} else {
+			var nextCh = this.peek();
+			var prevCh = number.charAt(number.length - 1);
+			if(ch === 'e' && this.isExpOperator(nextCh)) {
+				number += ch;
+			} else if(this.isExpOperator(ch) && prevCh === 'e' && nextCh && this.isNumber(nextCh)) {
+				number += ch;
+			} else if(this.isExpOperator(ch) && prevCh === 'e' && (!nextCh || !this.isNumber(nextCh))) {
+				throw 'Invalid exponent';
+			} else {
+				break;
+			}
+		}
+		this.index++;
+	}
+
+	this.tokens.push({
+		text: number,
+		value: Number(number)
+	});
+};
+
+Lexer.prototype.peek = function() {
+	return this.index < this.text.length - 1 ?
+		this.text.charAt(this.index + 1) :
+		false;
+};
+
+Lexer.prototype.isExpOperator = function(ch) {
+	return ch === '-' || ch === '+' || this.isNumber(ch);
+};
+
+/* 对string进行词法分析，提供给AST builder */
+Lexer.prototype.readString = function(quote) {
+	this.index++;
+	var string = '';
+	var escape = false;
+
+	while(this.index < this.text.length) {
+		var ch = this.text.charAt(this.index);
+
+		if(escape) {
+			if(ch === 'u') {
+				var hex = this.text.substring(this.index + 1, this.index + 5);
+				if(!hex.match(/[\da-f]{4}/i)) {
+					throw 'Invalid unicode escape';
+				}
+				this.index += 4;
+				string += String.fromCharCode(parseInt(hex, 16));
+			} else {
+				var replacement = ESCAPES[ch];
+				if(replacement) {
+					string += replacement;
+				} else {
+					string += ch;
+				}
+			}
+
+			escape = false;
+		} else if(ch === quote) {
+			this.index++;
+			this.tokens.push({
+				text: string,
+				value: string
+			});
+			return;
+
+		} else if(ch === '\\') {
+			escape = true;
+
+		} else {
+			string += ch;
+		}
+
+		this.index++;
+	}
+
+	throw 'Unmatched quote';
+};
+
+Lexer.prototype.isIdent = function(ch) {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
+		ch === '_' || ch === '$';
+};
+
+/* 对标识符的词法分析 */
+Lexer.prototype.readIdent = function() {
+	var text = '';
+	while(this.index < this.text.length) {
+		var ch = this.text.charAt(this.index);
+		if(this.isIdent(ch) || this.isNumber(ch)) {
+			text += ch;
+		} else {
+			break;
+		}
+
+		this.index++;
+	}
+
+	var token = {
+		text: text
+	};
+
+	this.tokens.push(token);
+};
+
+Lexer.prototype.isWhitespace = function(ch) {
+	return ch === ' ' || ch === '\r' || ch === '\t' || ch === '\n' || ch === '\v' || ch === '\u00A0';
+};
+
+
+
+
+/* AST builder
+   接收Lexer输出的tokens，生成抽象语法树
+ */
+function AST(lexer) {
+	this.lexer = lexer;
+}
+AST.Program = 'Program';
+AST.Literal = 'Literal';
+AST.ArrayExpression = 'ArrayExpression';
+
+AST.prototype.constants = {
+	'null': {
+		type: AST.Literal, value: null
+	},
+	'true': {
+		type: AST.Literal, value: true
+	},
+	'false': {
+		type: AST.Literal, value: false
+	}
+};
+
+AST.prototype.ast = function(text) {
+	this.tokens = this.lexer.lex(text);
+	return this.program();
+};
+
+AST.prototype.program = function() {
+	return {
+		type: AST.Program,
+		body: this.primary()
+	};
+};
+
+AST.prototype.primary = function() {
+	if(this.expect('[')) {
+		return this.arrayDeclaration();
+	} else if(this.constants.hasOwnProperty(this.tokens[0].text)) {
+		return this.constants[this.tokens[0].text];
+	} else {
+		return this.constant();
+	}
+};
+
+AST.prototype.constant = function() {
+	return {
+		type: AST.Literal, value: this.tokens[0].value
+	};
+};
+
+AST.prototype.expect = function(e) {
+	if(this.tokens.length > 0) {
+		if(this.tokens[0].text === e || !e) {
+			return this.tokens.shift();
+		}
+	}
+};
+
+AST.prototype.arrayDeclaration = function() {
+	this.consume(']');
+	return {
+		type: AST.ArrayExpression
+	};
+};
+
+AST.prototype.consume = function(e) {
+	var token = this.expect(e);
+	if(!token) {
+		throw 'Unexpected. Expecting: ' + e;
+	}
+	return token;
+};
+
+/* AST compiler
+   根据AST, 生成用于执行的函数
+ */
+function ASTCompiler(astBuilder) {
+	this.astBuilder = astBuilder;
+
+}
+
+ASTCompiler.prototype.compile = function(text) {
+	var ast = this.astBuilder.ast(text);
+	this.state = {
+		body: []
+	};
+	this.recurse(ast);
+
+	/* jshint -W054 */
+	return new Function(this.state.body.join(''));
+	/* jshint +W054 */
+};
+
+ASTCompiler.prototype.recurse = function(ast) {
+	switch(ast.type) {
+		case AST.Program:
+			this.state.body.push('return ', this.recurse(ast.body), ';');
+			break;
+		case AST.Literal:
+			return this.escape(ast.value);
+		case AST.ArrayExpression:
+			return '[]';
+	}
+};
+
+ASTCompiler.prototype.escape = function(value) {
+	if(_.isString(value)) {
+		return '\'' + value.replace(this.stringEscapeRegex, this.stringEscapeFn) + '\'';
+	} else if(_.isNull(value)) {
+		return 'null';
+	} else {
+		return value;
+	}
+};
+
+ASTCompiler.prototype.stringEscapeRegex = /[^ a-zA-Z0-9]/g;
+
+ASTCompiler.prototype.stringEscapeFn = function(c) {
+	return '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4);
+};
+
+
+/* parser 完成表达式的函数执行 */
+function Parser(lexer) {
+	this.lexer = lexer;
+	this.ast = new AST(this.lexer);
+	this.ASTCompiler = new ASTCompiler(this.ast);
+}
+
+Parser.prototype.parse = function(text) {
+	return this.ASTCompiler.compile(text);
+};
+
+
+/* parse method 总方法 */
+function parse(expr) {
+	var lexer = new Lexer();
+	var parser = new Parser(lexer);
+	return parser.parse(expr);
+}
+
+module.exports = parse;
+
+
+
+
+
+
+
+
+
+
